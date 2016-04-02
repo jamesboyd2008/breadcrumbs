d9c52a1b91fbbabdf5a8ad6875d6a475859f120d
jjschnei <jeffjschneider@gmail.com>
2016-04-02 10:23:23 -0700

    Create main and index files

--- react-n/breadcrumbs/index.ios.js
+++ react-n/breadcrumbs/index.ios.js
@@ -1,31 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+var React = require('react-native')
+var Main = require('./app/Components/Main')
 
-import React, {
+
+var {
+  NavigatorIOS,
   AppRegistry,
   Component,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} = React;
 
 class breadcrumbs extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        initialRoute={{
+          title: "Breadcrumbs",
+          component: Main,
+        }} />
     );
   }
 }
